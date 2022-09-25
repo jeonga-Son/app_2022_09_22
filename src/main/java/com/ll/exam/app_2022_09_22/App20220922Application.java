d87d42feac05a61830f4a8ea87271257d41b511a
jeonga <jjeong0106@gmail.com>
2022-09-25 11:19:14 +0900

    스프링부트앱을 배치앱으로 설정하여 관련 스키마가 생기도록 구현

--- src/main/java/com/ll/exam/app_2022_09_22/App20220922Application.java
+++ src/main/java/com/ll/exam/app_2022_09_22/App20220922Application.java
@@ -1,9 +1,12 @@
 package com.ll.exam.app_2022_09_22;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// @EnableBatchProcessing 어노테이션을 안붙이면 배치앱이 아니다. 배치로서의 작업을 하지 않는다.
+@EnableBatchProcessing
 public class App20220922Application {
 
 	public static void main(String[] args) {
