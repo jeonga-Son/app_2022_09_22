84aaff7e75ab0e0189718dc5ba6c572b0500fcd7
jeonga <jjeong0106@gmail.com>
2022-09-26 13:50:52 +0900

    장바구니 구현

--- src/main/java/com/ll/exam/app_2022_09_22/app/base/DevInitData.java
+++ src/main/java/com/ll/exam/app_2022_09_22/app/base/DevInitData.java
@@ -1,7 +1,9 @@
 package com.ll.exam.app_2022_09_22.app.base;
 
+import com.ll.exam.app_2022_09_22.app.cart.service.CartService;
 import com.ll.exam.app_2022_09_22.app.member.entity.Member;
 import com.ll.exam.app_2022_09_22.app.member.service.MemberService;
+import com.ll.exam.app_2022_09_22.app.product.entity.Product;
 import com.ll.exam.app_2022_09_22.app.product.entity.ProductOption;
 import com.ll.exam.app_2022_09_22.app.product.service.ProductService;
 import org.springframework.boot.CommandLineRunner;
@@ -19,7 +21,7 @@ public class DevInitData {
     @Bean
     // CommandLineRunner 인터페이스는 구동 시점에 실행되는 코드가 자바 문자열 아규먼트 배열에 접근해야할 필요가 있는 경우에 사용한다.
     // 프로그램이 다 로깅된 이후 실행된다.
-    public CommandLineRunner initData(MemberService memberService, ProductService productService) {
+    public CommandLineRunner initData(MemberService memberService, ProductService productService, CartService cartService) {
         return args ->
         {
             String password = "{noop}1234";
@@ -28,9 +30,14 @@ public class DevInitData {
             Member member3 = memberService.join("user3", password, "user3@test.com");
             Member member4 = memberService.join("user4", password, "user4@test.com");
 
-            productService.create("단가라 OPS", 68000, "청평화 A-1-15", Arrays.asList(new ProductOption("RED", "44"), new ProductOption("RED", "55"), new ProductOption("BLUE", "44"), new ProductOption("BLUE", "55")));
-            productService.create("쉬폰 OPS", 72000, "청평화 A-1-15", Arrays.asList(new ProductOption("BLACK", "44"), new ProductOption("BLACK", "55"), new ProductOption("WHITE", "44"), new ProductOption("WHITE", "55")));
+            Product product1 = productService.create("단가라 OPS", 68000, "청평화 A-1-15", Arrays.asList(new ProductOption("RED", "44"), new ProductOption("RED", "55"), new ProductOption("BLUE", "44"), new ProductOption("BLUE", "55")));
+            Product product2 = productService.create("쉬폰 OPS", 72000, "청평화 A-1-15", Arrays.asList(new ProductOption("BLACK", "44"), new ProductOption("BLACK", "55"), new ProductOption("WHITE", "44"), new ProductOption("WHITE", "55")));
 
+            ProductOption productOption__RED_44 = product1.getProductOptions().get(0);
+            ProductOption productOption__BLUE_44 = product1.getProductOptions().get(2);
+            cartService.addItem(member1, productOption__RED_44, 1); // productOption__RED_44 총 수량 1
+            cartService.addItem(member1, productOption__RED_44, 2); // productOption__RED_44 총 수량 3
+            cartService.addItem(member1, productOption__BLUE_44, 1); // productOption__BLUE_44 총 수량 1
         };
     }
 }
